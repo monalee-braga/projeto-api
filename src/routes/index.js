9304758339b7690da57625761709f1f16f7ded98
Mona-Lee Braga <codewith.monalee@gmail.com>
2022-11-28 19:33:37 -0300

    sintetizando as rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import products from './productsRoutes.js';
 import users from './usersRoutes.js';
 import auth from './authRoutes.js';
 import authentication from '../auth/authentication.js';
@@ -9,7 +8,7 @@ const routes = (app) => {
     res.status(200).send('Curso de Node');
   });
 
-  app.use(express.json(), auth, users, products);
+  app.use(express.json(), auth, users);
 };
 
 export default routes;
