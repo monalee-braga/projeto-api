e0f173e8348dacf51d4328f3073b94c2f169b6f8
Mona-Lee Braga <codewith.monalee@gmail.com>
2023-01-10 19:34:14 -0300

    modificando model de usuários

--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,45 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-  id: { type: String },
-  name: { type: String, required: true },
-  email: { type: String, unique: true, required: true, lowercase: true },
-  password: { type: String, required: true, select: false },
-  permission: { type: String, required: true },
-  telephone: { type: String },
-  createAt: { type: Date, default: Date.now }
+  id: { 
+    type: String 
+  },
+  name: { 
+    type: String, 
+    required: true 
+  },
+  email: { 
+    type: String, 
+    unique: [true, "email already exists in database!"],
+    lowercase: true,
+    trim: true,
+    required: [true, "email not provided"],
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: '{VALUE} is not a valid email!'
+    }
+  },
+  password: { 
+    type: String, 
+    required: true, 
+    select: false 
+  },
+  permission: { 
+    type: String, 
+    enum: ["normal", "admin"],
+    required: [true, "Please specify user permission"]
+  },
+  telephone: { 
+    type: String 
+  },
+  createAt: { 
+    type: Date, 
+    default: Date.now 
+  }
 })
 
-const user = mongoose.model('user', userSchema)
+const user = mongoose.model('User', userSchema)
 
 export default user
